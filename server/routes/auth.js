8015f4e61676f4a01298d86a851d0a17ebfa0e4b
Eri Irawan <email": null>
2017-03-10 14:17:01 +0700

    web token + passport login facebook

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,14 +2,22 @@
 
 const express = require('express')
 const router = express.Router()
-
+const passport = require('passport')
+const auth = require('../authentication')
+const jwt = require('jsonwebtoken')
+const user = require('../controllers/userController')
 
 router.get('/facebook', passport.authenticate('facebook'))
 
-router.get('/facebook/callback',
-  passport.authenticate('facebook' {
-    failureRedirect: 'index/login'
-  }), (req, res) => {
-    res.redirect('index/home')
-  }
-)
+router.get('/facebook/callback', passport.authenticate('facebook'), user.signIn)
+
+module.exports = router
+
+// , {
+//   failureRedirect: 'index/login'
+// }), (req, res) => {
+//   console.log(res)
+//   console.log(res.req)
+//   var token = jwt.sign({}, 'GwGanteng')
+//   res.send('sucsess')
+// }
