988ae250378a506f3d84cf4ce02fd232133e324b
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-03-10 11:33:40 +0700

    Fix db.js

--- server/db.js
+++ server/db.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 
 let mongoDB = `mongodb://${process.env.HOST_NAME}/${process.env.DATABASE_NAME}`
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 mongoose.connect(mongoDB)
 
 let db = mongoose.connection
 
-db.on('error', console.error.bind(console, 'MongoDB Connection error');)
+db.on('error', console.error.bind(console, 'MongoDB Connection error'))
