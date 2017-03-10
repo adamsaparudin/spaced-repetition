5196f79dfd884be950063dc13d3df4c878004c1e
Danang Aji Tamtomo <imsuretamtomo@gmail.com>
2017-03-10 11:37:05 +0700

    Fix export module of apis router

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,10 @@
 'use strict'
 
-const express = require('express')
-const router = express.Router()
-
-router.get('/facebook', passport.authenticate('facebook'))
-
-router.get('/facebook/callback',
-  passport.authenticate('facebook')
-)
+// const express = require('express')
+// const router = express.Router()
+//
+// router.get('/facebook', passport.authenticate('facebook'))
+//
+// router.get('/facebook/callback',
+//   passport.authenticate('facebook')
+// )
